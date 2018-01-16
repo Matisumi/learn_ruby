a6b355f39b49f4f7969a483af1d20ae45ee5c91b
Sabine <matisuo@gmail.com>
2018-01-16 13:56:45 +0100

    calculator ex -- missing bonus

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,23 @@
 #write your code here
+def add(nb1, nb2)
+	nb1 + nb2
+end
+
+def subtract(nb1, nb2)
+	nb1 - nb2
+end
+
+def sum(arr)
+	res = 0
+	arr.each do |x|
+		res = res + x
+	end
+	return res
+end
+
+
+=begin
+def multiply (nb1, nb2)
+	n1 * nb2
+end
+=end
