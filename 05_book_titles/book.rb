25d239064cfb97c8dcf7bebb6c81fc2dcf5e549e
Sabine <matisuo@gmail.com>
2018-01-16 22:22:46 +0100

    adding book exercice

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,25 @@
 class Book
 # write your code here
+	def title
+		@title
+	end
+
+	def title= (title)
+		@title = capitalize(title)
+	end
+
+
+	def capitalize(title)
+	  little_words = ["the", "and", "in", "of", "a", "an"]
+    words = title.split(' ')
+
+    words[0].capitalize!
+    words.each do |word|
+    	unless little_words.include?(word)
+    		word.capitalize!
+      end
+    end
+
+    return words.join(" ")
+	end
 end
